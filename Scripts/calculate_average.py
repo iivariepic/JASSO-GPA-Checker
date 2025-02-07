c0861130ebc8ce48f81ab2c3b7692182a1ce6601
Iivari Anttila <iivari03@gmail.com>
2025-02-07 15:01:03 +0200

    Made function to calculate average grade

---
+++ Scripts/calculate_average.py
@@ -0,0 +1,13 @@
+# Function to calculate the average grade
+from Scripts.grade import Grade
+
+
+def calculate_average(grades:list[Grade]) -> float:
+    credit_amount:int = 0
+    grade_amount_weighted:int = 0
+
+    for grade in grades:
+        credit_amount += grade.credit_amount
+        grade_amount_weighted = grade.JASSO_grade * grade.credit_amount
+
+    return grade_amount_weighted/credit_amount
